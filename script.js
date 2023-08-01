4b5cce2fcdf4087e7b57e77f510e453931598bc7
Sandeep Ichake <sandeep.ichake@gmail.com>
2023-08-01 22:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function calculateTime() {
   let date = new Date();
   let dayNumber = date.getDay();
   let hour = date.getHours();
-  let min = date.getMinutes();
+  let minute = date.getMinutes();
   let time = hour >= 12 ? "PM" : "AM";
   let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   hour = hour % 12;
   hour = hour ? hour : "12";
   hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0" + min : min;
+  minute = minute < 10 ? "0" + minute : minute;
 
   document.getElementById("day").innerHTML = days[dayNumber];
   document.getElementById("hour").innerHTML = hour;
-  document.getElementById("min").innerHTML = min;
+  document.getElementById("min").innerHTML = minute;
   document.getElementById("time").innerHTML = time;
 
   setTimeout(calculateTime, 200);
